d56152cfe630ea7437b0fd81963a1675e97a0ea1
vivaldev <valtteri.virtanen.dev@gmail.com>
2022-11-10 19:52:14 +0200

    First_commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Charts from "./pages/Charts";
 import Search from "./pages/Search";
+import Stats from "./pages/Stats";
+import Coins from "./pages/Coins";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -14,19 +17,14 @@ const App = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C200d%2C1y"
-      )
-      .then((response) => {
-        setData(response.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError(error);
-        setLoading(false);
-      });
+    fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    )
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.log(err));
   }, []);
+  console.log(data);
 
   return (
     <div className="App">
@@ -35,6 +33,8 @@ const App = () => {
         <Route path="/home" element={<Home data={data} />} />
         <Route path="/charts" element={<Charts data={data} />} />
         <Route path="/search" element={<Search data={data} />} />
+        <Route path="/stats" element={<Stats data={data} />} />
+        <Route path="/coins" element={<Coins data={data} />} />
       </Routes>
     </div>
   );
