d6e1631608ac675a816c7ebf535466f9e26b5a67
vivaldev <valtteri.virtanen.dev@gmail.com>
2022-11-13 14:58:37 +0200

    Added login notice box to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,12 +14,36 @@ import { useDisclosure } from "@chakra-ui/react";
 
 function Home({ data }) {
   return (
-    <div>
-      <Heading as="h3" size="lg" color="brand.800" fontFamily="Karla">
-        {" "}
-        Home
-      </Heading>
-    </div>
+    <Box>
+      <Flex w="100%" h="100vh" justify="center">
+        {/* LOGIN NOTICE BOX */}
+
+        <Box bg="brand.200" w="40%" h="30vh" p="5" borderRadius="md" mt={40}>
+          <Flex
+            justify="center"
+            align="center"
+            w="100%"
+            h="30vh"
+            direction="column"
+          >
+            <Heading as="h3" size="lg">
+              You haven't signed in yet.
+            </Heading>
+            <Text fontSize="lg" mt="5">
+              Please Login or Register User
+            </Text>
+            <Flex mt={10} justify="space-between" w="65%">
+              <Button bg="brand.100" p={5} fontSize="lg">
+                Login
+              </Button>
+              <Button bg="brand.100" p={5} fontSize="lg">
+                Register user
+              </Button>
+            </Flex>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
   );
 }
 
